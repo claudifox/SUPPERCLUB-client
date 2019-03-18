61a06d88fe1968cf9ee649e096228780cff30833
Claudia Fox <claudiafox95@icloud.com>
2019-03-18 10:26:23 +0000

    Removed Location Search Input Component and added to New Supper Form

--- src/containers/NewSupperForm.js
+++ src/containers/NewSupperForm.js
@@ -1,6 +1,48 @@
 import React, { Component } from 'react'
+import LocationSearchInput from '../components/LocationSearchInput'
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from 'react-places-autocomplete';
+
+
 
 export default class NewSupperForm extends Component {
+
+    state = {
+      name: "",
+      date: "",
+      time: "",
+      address: "",
+      selectedAddressLngLat: "",
+      selectedAddress: "",
+    }
+
+    handleChange = event => {
+      this.setState({[event.target.name]: event.target.value})
+    }
+
+    handleAddressChange = address => {
+      this.setState({ address });
+    };
+
+    handleSelect = address => {
+      this.setState({
+        selectedAddress: address });
+      geocodeByAddress(address)
+        .then(results => getLatLng(results[0]))
+        .then(latLng => {
+          this.setState({
+            selectedAddressLngLat: latLng
+          })
+          console.log('Success', latLng)
+        })
+        .catch(error => console.error('Error', error));
+    };
+
+    handleSubmit = event => {
+
+    }
 
   render() {
     return (
@@ -9,20 +51,48 @@ export default class NewSupperForm extends Component {
         <form>
         <label className="label">
           NAME
-          <input className="input" type="text" name="where" />
+          <input className="input" type="text" name="name" value ={this.state.name} onChange={this.handleChange} />
         </label>
-          <label className="label">
-            DATE
-            <input className="input" type="text" name="date" />
-          </label>
-          <label className="label">
-            TIME
-            <input className="input" type="text" name="time" />
-          </label>
-          <label className="label">
-          WHERE
-          <input className="input" type="text" name="where" />
-          </label>
+          <label className="label">DATE </label>
+          <input className="input" type="date" name="date" onChange={this.handleChange} />
+
+          <label className="label">TIME </label>
+          <input className="input" type="time" name="time" onChange={this.handleChange} />
+
+          <label className="label">WHERE</label>
+          <PlacesAutocomplete
+            value={this.state.address}
+            onChange={this.handleAddressChange}
+            onSelect={this.handleSelect}
+          >
+            {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+              <div>
+                <input
+                  {...getInputProps({
+                    placeholder: 'Search Places ...',
+                    className: 'location-search-input',
+                  })}
+                />
+                <div className="autocomplete-dropdown-container">
+                  {loading && <div>Loading...</div>}
+                  {suggestions.map(suggestion => {
+                    const className = suggestion.active
+                      ? 'suggestion-item--active'
+                      : 'suggestion-item';
+                    return (
+                      <div
+                        {...getSuggestionItemProps(suggestion, {
+                          className,
+                        })}
+                      >
+                        <span>{suggestion.description}</span>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            )}
+          </PlacesAutocomplete>
           <input type="submit" value="Create New Supper" />
         </form>
       </div>
